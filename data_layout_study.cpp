a0ca8b5e471dd24da83d3b351535a0e376d92f16
Geoffrey Viola <geoffrey.viola@asirobots.com>
2017-05-31 23:32:48 -0700

    initial attemp

---
+++ data_layout_study.cpp
@@ -0,0 +1,40 @@
+#include <cstdint>
+#include <iostream>
+
+using std::cout;
+/*
+struct EightBools
+{
+  bool one;
+  bool two;
+  bool three;
+  bool four;
+  bool five;
+  bool six;
+  bool seven;
+  bool eight;
+};
+
+struct DanglingEnd
+{
+  uint32_t first;
+  uint8_t end;
+};
+struct Sandwich
+{
+  uint32_t first;
+  uint8_t middle;
+  uint32_t end;
+};
+
+*/
+int main()
+{
+  cout << "data layout study start" << '\n';
+  //cout << "size of eight bools = " << sizeof(EightBools) << '\n'; 
+  //cout << "size of dangling end = " << sizeof(DanglingEnd) << '\n'; 
+  //cout << "size of sandwich= " << sizeof(Sandwich) << '\n'; 
+  cout << "data layout study end" << '\n';
+  return EXIT_SUCCESS;
+}
+
