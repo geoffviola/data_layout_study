d40a51b81279c97fd4a594073800836cb2dae2d5
Geoffrey Viola <geoffviola@users.noreply.github.com>
2017-06-01 22:31:44 -0700

    pragmas work better than flags

--- data_layout_study.cpp
+++ data_layout_study.cpp
@@ -3,6 +3,12 @@
 
 using std::cout;
 
+#if defined(__llvm__) || defined(__GNUC__)
+#define PACK( __Declaration__ ) __Declaration__ __attribute__((__packed__))
+#else
+#define PACK( __Declaration__ ) __pragma( pack(push, 1) ) __Declaration__ __pragma( pack(pop) )
+#endif
+
 struct EightBools
 {
   bool one;
@@ -20,6 +26,7 @@ struct DanglingEnd
   uint32_t first;
   uint8_t end;
 };
+
 struct Sandwich
 {
   uint32_t first;
@@ -27,12 +34,46 @@ struct Sandwich
   uint32_t end;
 };
 
+
+PACK(
+struct EightBoolsPacked
+{
+  bool one;
+  bool two;
+  bool three;
+  bool four;
+  bool five;
+  bool six;
+  bool seven;
+  bool eight;
+});
+
+PACK(
+struct DanglingEndPacked
+{
+  uint32_t first;
+  uint8_t end;
+});
+
+PACK(
+struct SandwichPacked
+{
+  uint32_t first;
+  uint8_t middle;
+  uint32_t end;
+});
+
+
 int main()
 {
   cout << "data layout study start" << '\n';
+  cout << "unpacked" << '\n';
   cout << "size of eight bools = " << sizeof(EightBools) << '\n'; 
   cout << "size of dangling end = " << sizeof(DanglingEnd) << '\n'; 
   cout << "size of sandwich= " << sizeof(Sandwich) << '\n'; 
+  cout << "size of eight bools = " << sizeof(EightBoolsPacked) << '\n'; 
+  cout << "size of dangling end = " << sizeof(DanglingEndPacked) << '\n'; 
+  cout << "size of sandwich= " << sizeof(SandwichPacked) << '\n'; 
   cout << "data layout study end" << '\n';
   return EXIT_SUCCESS;
 }
